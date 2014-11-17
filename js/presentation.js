e3a9a298d64c76b446adde90f2ac76fba5a7a7a2
Kevin <kevin.denver@gmail.com>
2014-11-17 23:56:09 +0000

    feat: added bespoke touch and hash

--- js/presentation.js
+++ js/presentation.js
@@ -1,5 +1,7 @@
 var deck = bespoke.from('article', [
     bespoke.themes.cube(),
     bespoke.plugins.keys(),
-    bespoke.plugins.backdrop()
+    bespoke.plugins.backdrop(),
+    bespoke.plugins.touch(),
+    bespoke.plugins.hash()
 ]);
