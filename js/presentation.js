65cbefb0a2688798bb1d0f72d7d49e40ec92f20f
Kevin <kevin.denver@gmail.com>
2014-11-18 01:12:36 +0000

    feat: fullscreen support and keyboard navigation

--- js/presentation.js
+++ js/presentation.js
@@ -1,7 +1,40 @@
+var keyboard = function() {
+
+    function launchIntoFullscreen(element) {
+        if (element.requestFullscreen) {
+            element.requestFullscreen();
+        } else if (element.mozRequestFullScreen) {
+            element.mozRequestFullScreen();
+        } else if (element.webkitRequestFullscreen) {
+            element.webkitRequestFullscreen();
+        } else if (element.msRequestFullscreen) {
+            element.msRequestFullscreen();
+        }
+    }
+
+    return function(deck) {
+        document.addEventListener('keydown', function(e) {
+
+            if (e.which == 80) { // P
+                deck.prev();
+            }
+
+            if (e.which == 78) { // N
+                deck.next();
+            }
+
+            if (e.which == 70) { // F
+            	launchIntoFullscreen(document.documentElement);
+            }
+        });
+    };
+};
+
 var deck = bespoke.from('article', [
     bespoke.themes.cube(),
     bespoke.plugins.keys(),
     bespoke.plugins.backdrop(),
     bespoke.plugins.touch(),
-    bespoke.plugins.hash()
+    bespoke.plugins.hash(),
+    keyboard()
 ]);
