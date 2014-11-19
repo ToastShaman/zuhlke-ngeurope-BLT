b79ee4670610e499b98a46424eb68315c7c004a4
Kevin <kevin.denver@gmail.com>
2014-11-19 15:58:39 +0000

    feat: added animas-css and query

--- js/presentation.js
+++ js/presentation.js
@@ -24,7 +24,6 @@ var keyboard = function() {
         });
 
         document.addEventListener('keydown', function(e) {
-
             if (e.which == 80) { // P
                 deck.prev();
             }
