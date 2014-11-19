cd6f24748e896b1d010917129a5adf520ff2d07b
Kevin <kevin.denver@gmail.com>
2014-11-19 19:20:54 +0000

    refactor: JS and consolidated CSS

--- js/presentation.js
+++ js/presentation.js
@@ -1,49 +1,44 @@
-var keyboard = function() {
+var mouse = function() {
+    return function(deck) {
+        registerMouse();
+    };
 
-    function launchIntoFullscreen(element) {
-        if (element.requestFullscreen) {
-            element.requestFullscreen();
-        } else if (element.mozRequestFullScreen) {
-            element.mozRequestFullScreen();
-        } else if (element.webkitRequestFullscreen) {
-            element.webkitRequestFullscreen();
-        } else if (element.msRequestFullscreen) {
-            element.msRequestFullscreen();
-        }
+    function registerMouse() {
+        document.addEventListener('mousedown', function(e) {
+            if (e.which == 1 /* Left Mouse */ ) deck.next();
+            if (e.which == 3 /* Left Mouse */ ) deck.prev();
+        });
+    }
+};
+
+var keyboard = function() {
+    return function(deck) {
+        registerKeys();
+    };
+
+    function registerKeys() {
+        document.addEventListener('keydown', function(e) {
+            if (e.which == 80 /* P */ ) deck.prev();
+            if (e.which == 78 /* N */ ) deck.next();
+            if (e.which == 70 /* F */ ) launchIntoFullscreen(document.documentElement);
+        });
     }
 
-    return function(deck) {
-        document.addEventListener('mousedown', function(e) {
-            // if (e.which == 1) { // Left? Mouse
-            //     deck.next();
-            // }
-
-            // if (e.which == 3) { // Right? Mouse
-            //     deck.prev();
-            // }
-        });
-
-        document.addEventListener('keydown', function(e) {
-            if (e.which == 80) { // P
-                deck.prev();
-            }
-
-            if (e.which == 78) { // N
-                deck.next();
-            }
-
-            if (e.which == 70) { // F
-                launchIntoFullscreen(document.documentElement);
-            }
-        });
-    };
+    function launchIntoFullscreen(element) {
+        if (element.requestFullscreen) element.requestFullscreen();
+        else if (element.mozRequestFullScreen) element.mozRequestFullScreen();
+        else if (element.webkitRequestFullscreen) element.webkitRequestFullscreen();
+        else if (element.msRequestFullscreen) element.msRequestFullscreen();
+    }
 };
 
 var deck = bespoke.from('article', [
     bespoke.themes.cube(),
+    bespoke.plugins.classes(),
     bespoke.plugins.keys(),
     bespoke.plugins.backdrop(),
     bespoke.plugins.touch(),
     bespoke.plugins.hash(),
+    //mouse(),
     keyboard()
 ]);
