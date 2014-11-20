293255cfa121e1babc44ac6fa4812fb9aa5cf458
Kevin <kevin.denver@gmail.com>
2014-11-20 22:44:10 +0000

    doc: more slides / fix: allow text selection

--- js/presentation.js
+++ js/presentation.js
@@ -1,3 +1,40 @@
+// bespoke-theme-cube.js
+var theme = function() {
+    return function(deck) {
+        var addClass = function(el, cls) {
+                el.classList.add('bespoke-' + cls);
+            },
+
+            removeClass = function(el, cls) {
+                el.className = el.className
+                    .replace(new RegExp('bespoke-' + cls + '(\\s|$)', 'g'), ' ')
+                    .trim();
+            },
+
+            deactivate = function(el, index) {
+                var activeSlide = deck.slides[deck.slide()],
+                    offset = index - deck.slide(),
+                    offsetClass = offset > 0 ? 'after' : 'before';
+
+                ['before(-\\d+)?', 'after(-\\d+)?', 'active', 'inactive'].map(removeClass.bind(null, el));
+
+                if (el !== activeSlide) {
+                    ['inactive', offsetClass, offsetClass + '-' + Math.abs(offset)].map(addClass.bind(null, el));
+                }
+            };
+
+        deck.slides.map(function(el) {
+            addClass(el, 'slide');
+        });
+
+        deck.on('activate', function(e) {
+            deck.slides.map(deactivate);
+            addClass(e.slide, 'active');
+            removeClass(e.slide, 'inactive');
+        });
+    };
+};
+
 var mouse = function() {
     return function(deck) {
         registerMouse();
@@ -33,7 +70,7 @@ var keyboard = function() {
 };
 
 var deck = bespoke.from('article', [
-    bespoke.themes.cube(),
+    theme(),
     bespoke.plugins.classes(),
     bespoke.plugins.keys(),
     bespoke.plugins.backdrop(),
